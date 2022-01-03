ccefd34d8d080d2293ca92bdd8f1dcd4b6ab0808
Courdevelops <courdevelops@gmail.com>
2022-01-03 14:29:29 -0500

    Basic Components without Links

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+        <h3>Courdevelops</h3>
+        <ul className='nav-links'>
+            <li>About</li> 
+            <li>More Info</li> 
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
