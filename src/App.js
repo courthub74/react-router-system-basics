0f58ed7d581d138f50f625cda42051abd5c77075
Courdevelops <courdevelops@gmail.com>
2022-01-03 15:39:53 -0500

    Routes added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
 import More from './components/More';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <div className="App">
+    
+    <Router>
+      <div className="App">
 
-      <Nav />
-      <About />
-      <More />
-     
-    </div>
+        <Nav />
+        {/* <About />
+        <More />
+        <Home /> */}
+        <Routes>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/more" component={More} />
+        </Routes>
+      
+      </div>
+    </Router>
+    
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
 
 export default App;
